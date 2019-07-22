ac46a8f5a6f0dda0e06d5bcc051f56ebb93e168d
DanTayar <dantayar28@gmail.com>
2019-07-22 15:44:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Europe from './Europe';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const colors = ['red','green', 'blue','orange','yellow','#fc0da1']
+
+
+class App extends React.Component {
+  state={}
+
+  changecountryColor= (event)=>{
+    const currentColorIndex = colors.indexOf(this.state[event.target.id] );
+    let nextColorIndex = currentColorIndex + 1 ;
+    // % colors.length
+    if(nextColorIndex === colors.length){ 
+      nextColorIndex=0;
+    }
+    const nextColor = colors[nextColorIndex]
+
+    this.setState({
+      [event.target.id]:nextColor
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Europe countryColors={this.state} onClick={this.changecountryColor}/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
